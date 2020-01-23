555b81ac6a8f9945519cd7d9b1cf397520ffe1e0
es <esrazengn00@gmail.com>
2020-01-23 11:29:04 +0100

    update baseUrl for heroku

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://react-reserve-eze.now.sh'
+    ? //? 'https://react-reserve-eze.now.sh'
+      'https://shielded-plateau-66265.herokuapp.com/'
     : 'http://localhost:3000';
 
 export default baseUrl;
